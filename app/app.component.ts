d4429fe1e3ff73955ce7bf1bd480f5a35a7cc759
Elliot Burin and Kim Bordon <elliot+kim@>
2017-12-04 16:13:16 -0800

    add two-way databinding and wild styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,12 +4,28 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-     <h3>{{currentFocus}}</h3>
-     <ul>
-        <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}  <button (click)="editTask()">Edit!</button></li>
+    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+    <ul>
+      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
     </ul>
- </div>
+    <hr>
+    <div>
+      <div *ngIf="selectedTask">
+        <h3>{{selectedTask.description}}</h3>
+        <p>Task Complete? {{selectedTask.done}}</p>
+        <hr>
+        <h3>Edit Task</h3>
+        <label>Enter Task Description:</label>
+        <input [(ngModel)]="selectedTask.description">
+        <label>Enter Task Priority (1-3):</label><br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+        <button (click)="finishedEditing()">Done</button>
+      </div>
+    </div>
+  </div>
   `
 })
 
@@ -22,12 +38,8 @@ export class AppComponent {
   tasks: Task[] = [
     new Task('Finish weekend Angular homework for Epicodus course', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
-    new Task('Add README file to last few Angular repos on GitHub', 2)
-  ];
-
-  editTask() {
-    alert("You just requested to edit a Task!");
-  }
+    new Task('Add README file to last few Angular repos on GitHub', 2)  ];
+  selectedTask = null;
 
   isDone(clickedTask: Task) {
     if(clickedTask.done === true) {
@@ -39,12 +51,20 @@ export class AppComponent {
 
   priorityColor(currentTask){
     if (currentTask.priority === 3){
-      return "bg-danger";
+      return "high";
     } else if (currentTask.priority === 2) {
-      return  "bg-warning";
+      return  "low";
     } else {
-      return "bg-info";
+      return "normal";
     }
+  }
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
   }
 }
 
